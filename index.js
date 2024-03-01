798ee01b7630c38d479465dd1c362d9b0af6b210
Jahid Anwer <jahid.englaa@gmail.com>
2024-03-01 20:01:43 +0600

    sorting functionality added

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const btnContainer = document.getElementById("btn-container");
+const cardContainer = document.getElementById("card-container");
+const sortBtn = document.getElementById("sort-btn");
 
 // Fetching categories
 const fetchCategories = async () => {
@@ -10,37 +12,105 @@ const fetchCategories = async () => {
   //   creating dynamic categories button
   categories.forEach((category) => {
     const btn = document.createElement("button");
-    btn.classList = `btn  btn-ghost bg-slate-700 text-white text-lg`;
-    btn.addEventListener("click", () => filterByCategory(category.category_id));
+    btn.classList = `btn category-btn  btn-ghost bg-slate-700 text-white text-lg`;
+    btn.addEventListener("click", () => {
+      fetchVideosByCategory(category.category_id);
+      const categoryBtns = document.getElementsByClassName("category-btn");
+      for (const btn of categoryBtns) {
+        btn.classList.remove("bg-red-600");
+      }
+      btn.classList.add("bg-red-600");
+    });
     btn.textContent = category.category;
     btnContainer.appendChild(btn);
   });
 };
 
 // Fetching videos by the categories
-const filterByCategory = async (id = 'all') => {
-  try {
+let categoryId = 1000;
+let isSorted = false;
+
+const fetchVideosByCategory = async (id, isSorted) => {
+  categoryId = id;
+  
+  
     const response = await fetch(
-      `https://openapi.programming-hero.com/api/videos/category/${id}`
+      `https://openapi.programming-hero.com/api/videos/category/${categoryId}`
     );
     const data = await response.json();
     const videos = data.data;
-    if (videos.length < 1) {
-      throw "Data not found";
+    // sorting videos by views 
+    if(isSorted){
+      videos.sort((a,b) => {
+        return parseFloat(b.others.views.replace('K', '')) - parseFloat(a.others.views.replace('K', ''));
+      })
     }
+    
     showVideos(videos);
-  } catch (error) {
-    const errorElement = document.getElementById('error-element');
-    errorElement.classList.remove('hidden');
-    console.error(error);
-  }
+
 };
+
+// sort videos by view
+sortBtn.addEventListener("click", () => {
+  isSorted = true;
+  fetchVideosByCategory(categoryId, isSorted);
+});
 
 // showing videos in ui
 const showVideos = (videos) => {
+  cardContainer.innerHTML = "";
+  const errorElement = document.getElementById("error-element");
 
-    console.log(videos)
-    
+  if (!videos.length > 0) {
+    errorElement.classList.remove("hidden");
+  } else {
+    errorElement.classList.add("hidden");
+    videos.forEach((video) => {
+      // console.log(video);
+      const card = document.createElement("div");
+      card.classList = `card w-full bg-base-100 shadow-xl`;
+      // card.addEventListener('click', showDetails(video))
+      card.innerHTML = `
+      <div onclick="showDetails('${video.title}')">
+        <figure class="overflow-hidden h-72">
+        <img class="w-full" src="${video.thumbnail}" alt="Shoes" />
+        <h6 class="absolute bottom-[40%] right-12">0 hr</h6>
+      </figure>
+        <div class="card-body">
+          <div class="flex space-x-4 justify-start items-start">
+            <div>
+              <img
+                class="w-12 h-12 rounded-full"
+                src="${video.authors[0].profile_picture}"
+                alt="Shoes"
+              />
+            </div>
+            <div>
+              <h2 class="card-title">${video.title}</h2>
+              <div class="flex mt-3">
+                <p class="">${video.authors[0].profile_name}</p>
+                ${
+                  video.authors[0].verified
+                    ? '<img class="w-6 h-6" src="./images/verify.png" alt="" />'
+                    : ""
+                }
+              </div>
+              <p class="mt-3"><span>${video.others.views}</span> Views</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+      cardContainer.appendChild(card);
+    });
+  }
 };
 
+// show details of single video 
+function showDetails(id) {
+  console.log(id);
+}
+
+// fetching categories buttons
 fetchCategories();
+fetchVideosByCategory(categoryId)
